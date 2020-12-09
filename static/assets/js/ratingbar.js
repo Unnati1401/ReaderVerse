e8e56cd149db2435cbeb7f5a132c30d4b99a4770
Unnati1401 <unnatinandrekar@gmail.com>
2020-12-09 20:10:43 +0530

    Ratings feature added, scroll bar fixed

--- static/assets/js/ratingbar.js
+++ static/assets/js/ratingbar.js
@@ -1,27 +1,22 @@
-var $star_rating;
-
-function setCurrentElement(elem){
-  $star_rating = elem;
+$("label").click(function(){
+    $(this).parent().find("label").css({"background-color": "#9a9191"});
+    $(this).css({"background-color": "#d9e719"});
+    $(this).nextAll().css({"background-color": "#d9e719"});
+});
+  
+// $('.ratings_stars').click(function() {
+//     $('.ratings_stars').removeClass('selected'); // Removes the selected class from all of them
+//     $(this).addClass('selected'); // Adds the selected class to just the one you clicked
+ 
+//     var rating = $(this).data('rating'); // Get the rating from the selected star
+//     $('#ratingID').val(rating); // Set the value of the hidden rating form element
+// });
+ 
+function setRating(id) {
+    $('.ratings_stars').removeClass('selected'); // Removes the selected class from all of them
+    $(this).addClass('selected'); // Adds the selected class to just the one you clicked
+ 
+    var rating = $(this).data('rating'); // Get the rating from the selected star
+    $('#id').val(rating); // Set the value of the hidden rating form element
+    console.log(id);
 }
-
-var SetRatingStar = function() {
-  return $star_rating.each(function() {
-    if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
-      return $(this).removeClass('fa-star-o').addClass('fa-star');
-    } else {
-      return $(this).removeClass('fa-star').addClass('fa-star-o');
-    }
-  });
-};
-
-$star_rating.on('click', function() {
-  $star_rating.siblings('input.rating-value').val($(this).data('rating'));
-  return SetRatingStar();
-});
-
-SetRatingStar();
-$(document).ready(function() {
-
-});
-
-
